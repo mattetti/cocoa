81beb00b956433e71698a62927e21c16c5124d3f
Matt Aimonetti <mattaimonetti@gmail.com>
2017-08-31 18:16:21 -0700

    happy bday

---
+++ bookmark.go
@@ -0,0 +1,63 @@
+package cocoa
+
+import (
+	"fmt"
+	"path/filepath"
+	"syscall"
+)
+
+/*
+	Cocoa provides an API to persist NSURLs. Older versions of the OS were using
+	alias records but that API was deprecated in favor of bookmarks. Cocoa does
+	also support symlinks and hardlinks but those behave differently than
+	bookmarks. Unfortunately, Apple doesn't document the Bookmark Data format.
+
+	Here is some documentation on the usage of bookmarks:
+	https://developer.apple.com/library/content/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/AccessingFilesandDirectories/AccessingFilesandDirectories.html#//apple_ref/doc/uid/TP40010672-CH3-SW10
+
+	The format was partly reverse engineered and documented in a few place, here
+	is the best summary I found:
+	https://github.com/al45tair/mac_alias/blob/master/doc/bookmark_fmt.rst
+	(documentation copied in doc/* in case the original repo disappears)
+*/
+
+// Bookmark acts like os.Symlink but instead of creating a symlink, a bookmark is stored.
+func Bookmark(src, dst string) error {
+	srcPath, err := filepath.Abs(src)
+	if err != nil {
+		return fmt.Errorf("failed to get the path of the source")
+	}
+	// read the attributes of the source.
+	var stat syscall.Statfs_t
+
+	err = syscall.Statfs(filepath.Clean(srcPath), &stat)
+	if err != nil {
+		return fmt.Errorf("failed to read the file stats - %s", err)
+	}
+
+	// Volume path
+	volPath := []byte{}
+	for _, b := range stat.Mntonname {
+		if b == 0x00 {
+			break
+		}
+		volPath = append(volPath, byte(b))
+	}
+	// fmt.Println(string(mntB))
+	buf := make([]byte, 308)
+	// attrs, err := GetAttrList(".", AttrList{CommonAttr: ATTR_CMN_FULLPATH}, buf, 0)
+	attrs, err := GetAttrList(string(volPath),
+		AttrListMask{
+			CommonAttr: ATTR_CMN_CRTIME,
+			VolAttr:    ATTR_VOL_SIZE | ATTR_VOL_NAME | ATTR_VOL_UUID,
+		},
+		buf,
+		0)
+	if err != nil {
+		fmt.Println(fmt.Errorf("failed to retrieve attribute list - %s", err))
+	}
+	fmt.Printf("%+v\n", attrs)
+
+	// attributes
+	return nil
+}
---
+++ cocoa.go
@@ -0,0 +1,5 @@
+// The cocoa package reimplements some native Cocoa features in pure Go so Go
+// programs running on Mac don't need to call into Cocoa. The goal of this
+// project is not to replace or cover all Cocoa APIs but to facilitate the work
+// of Gophers on Mac.
+package cocoa
